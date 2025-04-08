1db731830158421cafe7078629065511c7b40da7
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-08 23:48:32 +0300

    Add TestimonialsSection component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import AboutSection from '../components/AboutSection';
 import FacilitiesGallery from '../components/FacilitiesGallery';
 import MembershipPlans from '../components/MembershipPlans';
 import TrainersSection from '../components/TrainersSection';
+import TestimonialsSection from '../components/TestimonialsSection';
 
 export default function HomePage() {
   return (
@@ -21,6 +22,7 @@ export default function HomePage() {
     <FacilitiesGallery />
     <MembershipPlans />
     <TrainersSection />
+    <TestimonialsSection />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
